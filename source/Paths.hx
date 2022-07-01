341071eaeb3293c40914dc8f16a6125df12a7eb2
Its-me-a-guy <102881579+Its-me-a-guy@users.noreply.github.com>
2022-07-01 11:42:49 -0300

    Update Paths.hx

--- source/Paths.hx
+++ source/Paths.hx
@@ -75,6 +75,11 @@ class Paths
 		return getPath('data/$key.txt', TEXT, library);
 	}
 
+	inline static public function txtOffsets(key:String, ?library:String)
+	{
+		return getPath('images/$key.txt', TEXT, library);
+	}
+
 	inline static public function xml(key:String, ?library:String)
 	{
 		return getPath('data/$key.xml', TEXT, library);
