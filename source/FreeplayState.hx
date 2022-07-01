7b8c097071ced919285581f4131436cd71205f73
Its-me-a-guy <102881579+Its-me-a-guy@users.noreply.github.com>
2022-07-01 12:14:06 -0300

    Update FreeplayState.hx

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -138,6 +138,10 @@ class FreeplayState extends MusicBeatState
 			trace(md);
 		 */
 
+                #if android
+                addVirtualPad(FULL, A_B);
+                #end
+
 		super.create();
 	}
 
