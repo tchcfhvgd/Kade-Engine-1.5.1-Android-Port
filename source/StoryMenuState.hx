ec90f0d10be8b0e96da5af09a7388cc359a5e233
Its-me-a-guy <102881579+Its-me-a-guy@users.noreply.github.com>
2022-07-01 11:58:25 -0300

    Update StoryMenuState.hx

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -196,6 +196,10 @@ class StoryMenuState extends MusicBeatState
 
 		trace("Line 165");
 
+                #if android
+                addVirtualPad(FULL, A_B);
+                #end
+
 		super.create();
 	}
 
