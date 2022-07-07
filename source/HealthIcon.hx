273a5e40f7aadc8c5f09ecc0f24a1223cc1dfef1
BoyfriendlolNewAccount <101530288+BoyfriendlolNewAccount@users.noreply.github.com>
2022-07-07 08:53:11 -0300

    Update HealthIcon.hx

--- source/HealthIcon.hx
+++ source/HealthIcon.hx
@@ -1,18 +1,6 @@
 package;
 
-import openfl.utils.Assets as OpenFlAssets; //Tr1ngle Engine
 import flixel.FlxSprite;
-import flixel.FlxBasic;
-import flixel.FlxCamera;
-import flixel.FlxG;
-import flixel.FlxGame;
-import flixel.FlxObject;
-import flixel.util.FlxColor;
-import lime.utils.Assets;
-#if cpp
-import sys.FileSystem;
-import sys.io.File;
-#end
 
 class HealthIcon extends FlxSprite
 {
@@ -20,14 +8,10 @@ class HealthIcon extends FlxSprite
 	 * Used for FreeplayState! If you use it elsewhere, prob gonna annoying
 	 */
 	public var sprTracker:FlxSprite;
-	public var isPlayer:Bool;
-	public var character:String;
 
 	public function new(char:String = 'bf', isPlayer:Bool = false)
 	{
 		super();
-		this.character = "";
-		this.isPlayer = isPlayer;
 		
 		loadGraphic(Paths.image('iconGrid'), true, 150, 150);
 
@@ -81,53 +65,6 @@ class HealthIcon extends FlxSprite
 
 		scrollFactor.set();
 	}
-
-                }
-		if(char != this.character)
-		{
-			#if desktop
-			if(FileSystem.exists(Paths.image("icons/icon-" + char)))
-			{
-				if(loadGraphics((Paths.image("icons/icon-" + char))).width >= 450)
-				{
-					loadGraphics((Paths.image("icons/icon-" + char)), true, 150, 150);
-					animation.add(char, [0, 1, 2], 0, false, this.isPlayer);
-				}
-				else if (loadGraphics((Paths.image("icons/icon-" + char))).width <= 300)
-				{
-					loadGraphics((Paths.image("icons/icon-" + char)), true, 150, 150);
-					animation.add(char, [0, 1], 0, false, this.isPlayer);
-				}
-				animation.play(char);
-				this.character = char;
-			}
-			else
-			{
-				changeIcon("face");
-			}
-			#else
-			if(Assets.exists(Paths.image("icons/icon-" + char)))
-			{
-				if(loadGraphics((Paths.image("icons/icon-" + char))).width >= 450)
-				{
-					loadGraphics((Paths.image("icons/icon-" + char)), true, 150, 150);
-					animation.add(char, [0, 1, 2], 0, false, this.isPlayer);
-				}
-				else if (loadGraphics((Paths.image("icons/icon-" + char))).width <= 300)
-				{
-					loadGraphics((Paths.image("icons/icon-" + char)), true, 150, 150);
-					animation.add(char, [0, 1], 0, false, this.isPlayer);
-				}
-				animation.play(char);
-				this.character = char;
-			}
-			else
-			{
-				changeIcon("face");
-			}
-			#end
-			
-		}
 
 	override function update(elapsed:Float)
 	{
